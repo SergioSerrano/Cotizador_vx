5969a884c754a98632bb8eba2bda44bc1493657f
Sergio Serrano Perez <pepes17@hotmail.com>
2013-10-17 12:01:10 -0500

    CAMBIAR ID DE CERRAR

--- js/API/eventos.js
+++ js/API/eventos.js
@@ -47,7 +47,7 @@ $(document).ready(function (e) {
 			
 			$linea.append($('<td></td>')
 				.attr({
-				id: 'posicion' + index+'12'
+				id: 'posicionC' + index+'12'
 			})
 				.html('')
 				.css({
@@ -132,7 +132,7 @@ $(document).ready(function (e) {
 			
 			$('<input  type="text" name="textinput" id="clv" value="" />').appendTo('#posicion' + index+'1');
 			$('<input  type="text" name="textinput" id="cnt" value="" />').appendTo('#posicion' + index + '2');			
-			$('<a  data-theme="b" href="#" data-transition="none" id="info_com">X</a>').appendTo('#posicion' + index+'12');
+			$('<a  data-theme="b" href="#" data-transition="none" id="info_com">X</a>').appendTo('#posicionC' + index+'12');
 			$('<a data-theme="c" href="#" data-transition="none" id="info_guarda">G</a>').appendTo('#posicion' + index + '6'); 
 			
 			index++;
